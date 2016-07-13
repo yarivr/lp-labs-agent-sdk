977b847e0b17a3edd386c245cb39b9237b0134c5
Yariv Rosenbach <yarivr@liveperson.com>
2016-07-13 17:42:29 +0300

    in progress

--- index.js
+++ index.js
@@ -9,30 +9,42 @@ var util = require('util');
 var SocketProtocol = require('./lib/ams/socket-protocol');
 
 
-class AgentSDK extends EventEmitter {
+class AgentSDK extends EventEmitter { // todo monitor the socket
     constructor(brandid, key, secret, lastUpdateTime) {
         // init brand-ws, subscribeEx
         // register, receive
+        super();
         this.brandid = brandid;
         this.key = key;
         this.secret = secret;
 
-        this.sp = new SocketProtocol(brandid, key, secret, amsUrl, adminAreaUrl, leUrl);
-        /*
-            Consumer -> Agent
-         */
-        sp.on('ams::data', function(err, data) {
-            var amsEvent = new AmsEvent(data);
-            this.emit(amsEvent.type, amsEvent.data); // consumer::ring, consumer::msg, consumer::accept, consumer::seen, consumer::compose, consumer::close
+        this.sp = new SocketProtocol(brandid, key, secret, 'wss://qatrunk.dev.lprnd.net',
+                'https://hc/s-qa51953286/web/m-LP/mlogin/home.jsp', 'https://qtvr-wap08.dev.lprnd.net/le/account/qa51953286/session');
+
+        this.sp.on('error', err => {
+           // TODO: ...
         });
+
+        this.sp.on('ws::connect', () =>  {
+            // in case of error close and re-create
+            //subscribeExConversations()
+            /*
+             Consumer -> Agent
+             */
+            this.sp.on('ams::data', data => {
+                var amsEvent = new AmsEvent(data);
+                this.emit(amsEvent.type, amsEvent.data); // consumer::ring, consumer::msg, consumer::accept, consumer::seen, consumer::compose, consumer::close
+            });
+
+        });
+
+
     }
 
     /*
         API's (Agent -> Consumer)
      */
-    subscribeExConversations() {
 
-    }
 
     acceptRing() {
 
@@ -59,9 +71,9 @@ class AgentSDK extends EventEmitter {
 
     }
 
-    updateTTR() {
-
-    }
+    //updateTTR() {
+    //
+    //}
 
     backToQueue() {
 
@@ -81,8 +93,9 @@ class AgentSDK extends EventEmitter {
     }
 
 }
-
-let as = new AgentSDK('qa6573138', 'bot@liveperson.com', '12345678', Date.now());
+/// https://hc/s-qa51953286/web/m-LP/mlogin/home.jsp
+//  https://qtvr-wap08.dev.lprnd.net/le/account/qa51953286/session
+let as = new AgentSDK('qa51953286', 'tester@lp.com', '12345678', Date.now());
 
 
 module.exports = AgentSDK;
